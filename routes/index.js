415177476118a3922b4e9114389e6e63ba554e42
Liang Aoi <aoi010122@becreator.com.tw>
2022-11-11 11:24:58 +0800

    stashs

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,20 @@ var router = express.Router();
 const cargoController = require("../controllers/cargo.controller")
 const userController = require("../controllers/user.controller")
 const platformController = require("../controllers/platform.controller")
+const fileController = require("../controllers/file.controller");
+const multer = require('multer');
+
+var upload = multer({
+  fileFilter: (req, file, cb) => {
+      if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg' ) {
+          cb(null, true)
+      } else {
+          cb(null, false)
+          return cb(new Error('Allowed .jpeg .jpg'))
+      }
+  }
+})
+
 const handleErrorAsync = require("../server/handleErrorAsync")
 // logIn
 router.post('/register', handleErrorAsync(userController.register));
@@ -21,5 +35,9 @@ router.post('/allCargos', handleErrorAsync(cargoController.findAll));
 router.patch("/updateCargo/:id" , handleErrorAsync(cargoController.updateCargo));
 router.delete("/delCargo/:id" , handleErrorAsync(cargoController.deleteOne));
 router.delete("/delAllCargos" , handleErrorAsync(cargoController.deleteAll));
+
+// file
+router.get('/allImages',fileController.allImages);
+router.post('/uploadImage',upload.single('file'),fileController.uploadImage)
 
 module.exports = router;
